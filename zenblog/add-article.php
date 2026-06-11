c83c8b84a34d7bb2c14997ceca8722c2c5a5e09a
Pha raoh <pharaoh1822005@gmail.com>
2026-06-11 21:16:59 +0600

    11.06.26 class material updated

--- zenblog/add-article.php
+++ zenblog/add-article.php
@@ -8,60 +8,50 @@
 
        
         
-     
-          <div class="col-lg-12">
-            <form  enctype="multipart/form-data" action="" method="post" class="php-email-form aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-              <div class="row gy-4">
+     <?php
+	 
+	 if(isset($_POST['submitBtn'])){
+		 $PostTitle = $_POST['PostTitle'];
+		 $PostCatagory = $_POST['PostCatagory'];
+		 $PostDiscription= $_POST['PostDiscription'];
+		 $cdate = date('d-m-Y');
+		 
+		 $pimg = ''.rand(00000,99999).'.png';
+		 
+		 
+		 move_uploaded_file($_FILES["PostImg"]["tmp_name"],'upload/'.$pimg.'');
 
-<?php
-//echo $cdate = date('d/m/Y');
-
-if(isset($_POST['submitBtn'])){
-$posttitle = $_POST['PostTitle'];
-$postdiscription = htmlentities($_POST['PostDiscription']);
-$postcatagory = $_POST['PostCatagory'];
-$cdate = date('d-m-Y');
-
-move_uploaded_file($_FILES["PostImg"]["tmp_name"],'upload/'.rand(00000,99999).'-'.rand(00000,99999).'.png');
-
-
-
-$sql = "INSERT INTO `posts` (`p_id`, `p_title`,`p_desc`,`p_users`,`p_date`,`p_category`,`p_status`,`p_link`)
- VALUES (NULL, '$posttitle','$postdiscription',NULL,'$cdate','$postcatagory',0,NULL)";
-if ($conn->query($sql) === TRUE) {
-  echo '<div class="alert alert-primary" role="alert">
-  Post Succesfully!
-</div>';
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-}
-
-?>
+		 
+		 
+		  $sql = "INSERT INTO `posts`( `p_title`, `p_desc`, `p_users`, `p_date`, `p_category`, `p_status`, `p_link`,`p_img`) VALUES ('$PostTitle','$PostDiscription','0','$cdate','$PostCatagory','0','','$pimg')";
+		if ($conn->query($sql) === TRUE) {
+		  echo '<div class="alert alert-primary" role="alert">
+		  Post Succesfully!
+		</div>';
+		} else {
+		  echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		 
+	 }
+	 
+	 
+	 ?>
 
 
-                
-                
-                  <input type="text" name="PostTitle" class="form-control" placeholder="title" required="">
-                  <input type="file" name="PostImg" class="form-control" placeholder="" required="">
-                
-				
-				 
-                  <textarea name="PostDiscription" rows= '5' class="form-control"></textarea>
-				  
-				  <input type="text" name="PostCatagory" class="form-control" placeholder="catagory" required="">
-              
-                <div class="col-md-12 text-center">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                  <input type="submit" name="submitBtn" />
-                </div>
-
-              </div>
-            </form>
-          </div><!-- End Contact Form -->
+		 
+		  <form  enctype="multipart/form-data" method="post">
+		  <h1>Add Article </h1>
+<input type="text" name="PostTitle" class="form-control m-2" placeholder="Post Title" required="">
+<input type="text" name="PostCatagory" class="form-control  m-2" placeholder="Post Category" required="">
+<textarea name="PostDiscription" rows= '5' class="form-control  m-2"></textarea>
+  <input type="file" name="PostImg" class="form-control   m-2" placeholder="" >
+    <input type="submit" class="btn btn-info   m-2" name="submitBtn" />             				  
+		  
+		  
+		  </form>
+		  
+		  
+		  
 
         </div>
 
